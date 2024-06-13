8abdfadde32be859183f52dfe1cc47fb98f5c66a
Julian-Lafond <jmlafond@ucsc.edu>
2024-06-13 09:10:45 -0700

    finished

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15" #must have headers otherwise http request fails
 }
 
+def extract_product_data(product):
+    try:
+        name = product.find('span', class_='productdescriptionname').text
+        brand = product.find('span', class_='productdescriptionbrand').text
+        price = product.find('span', class_='CurrencySizeLarge curprice').text.strip()
+        print(f'Brand: {brand} Name: {name} Price: {price}')
+    except Exception as e:
+        print(f'Error extracting product data: {e}')
+
+
+
 
 def main():
     response = httpx.get(url, headers=headers)
     response_html = response.text
     
     soup = BeautifulSoup(response_html, 'html.parser')
-    print(soup.find_all('a'))   #find all a tags
-
-
+    products = soup.find_all('div', class_='s-productthumbbox')
+    for product in products:
+        extract_product_data(product)
+        
 if __name__=='__main__':
     main()
